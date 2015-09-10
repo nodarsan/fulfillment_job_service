e1a18636c52c45b9c5267c2b8a8456f6d0af880c
Federico Zuppa <fzuppa@fede.10pines.net>
2015-09-10 15:51:12 -0300

    Send complex type in request

--- app/services/fulfillment_job/service.rb
+++ app/services/fulfillment_job/service.rb
@@ -1,20 +1,26 @@
+require 'money'
 module FulfillmentJob
   class Service
     def create(printer_id, name, cost)
+      puts Money.from_amount(cost['value'])
       job = {
           id: 1,
           printer_id: printer_id,
           name: name,
           status: 'active',
-          cost: cost,
+          cost: Money.from_amount(cost['value'], cost['currency'] || 'USD'),
       }
       serialize(job)
+    end
+
+    def show_exception
+      raise "Esto es una exception!!"
     end
 
     private
 
     def serialize(job)
-      job.stringify_keys
+      job.merge({cost: {"value": 12}.stringify_keys}).stringify_keys
     end
   end
 end
--- client/client.rb
+++ client/client.rb
@@ -3,4 +3,4 @@ require 'barrister'
 trans = Barrister::HttpTransport.new("http://localhost:3000/fulfillment_job")
 client = Barrister::Client.new(trans)
 
-puts client.FulfillmentJobSystem.create(2, "name", 2.14)
+puts client.FulfillmentJobSystem.create(2, "name", {"value" => 23})
